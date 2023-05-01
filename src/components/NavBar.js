4b0e96a9ba73def19a505dc0d2cdd778617f6b7b
George Samuel <geosam2708@gmail.com>
2023-05-01 20:21:07 +0300

    changed navbar to render as list

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,26 +3,34 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return <div className="navbar">
+    <li>
     <NavLink
     to = "/"
     exact >
       Home
     </NavLink>
+    </li>
+    <li>
     <NavLink
     to = "/movies"
     exact >
       Movies
     </NavLink>
+    </li>
+    <li>
     <NavLink
     to = "/directors"
     exact >
       Directors
     </NavLink>
+    </li>
+    <li>
     <NavLink
     to = "/actors"
     exact >
       Actors
     </NavLink>
+    </li>
   </div>;
 }
 
